5298a0a9e3d63328bce68d7e16dc796e5f7932b0
kristianto wijaya <k.wijaya@verihubs.com>
2024-10-19 17:11:46 +0700

    add register button

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Login() {
   return (
-    <div className='flex bg-white w-4/5 h-1/2 m-8 p-5 rounded-2xl md:p-8 md:m-10 md:w-3/4'>
-      <form action="" className='flex-col min-w-full min-h-full'>
+    <div className='flex flex-col bg-white w-4/5 h-2/5 m-8 p-5 rounded-2xl md:p-8 md:m-10 md:w-3/4'>
+      <form action="" className='min-w-full min-h-3/4'>
         <div className='min-w-full mb-8'>
           <label htmlFor="username">Username</label>
           <input type="text" name='username' placeholder='username' className='block mt-2 border-1 rounded-xl bg-transparent py-1.5 pl-2 w-full text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'/>
@@ -16,6 +17,10 @@ export default function Login() {
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign In</button>
         </div>
       </form>
+      <div className='border-indigo-200 border-b-2 mt-5'></div>
+      <div className='flex justify-center mt-5 text-lg'>
+        <span>Doesn't have account yet? <Link href="./register" className='text-blue-500'>Sign Up</Link></span>
+      </div>
     </div>
   )
 }
